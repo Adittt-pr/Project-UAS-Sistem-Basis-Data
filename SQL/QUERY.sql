0b71e3fc03df7c2b5d51d6485cd72f48f327b6ec
Neco <adnanmuhammad0605@gmail.com>
2026-01-18 19:34:08 +0700

    QUERY.sql

--- SQL/QUERY.sql
+++ SQL/QUERY.sql
@@ -2,6 +2,51 @@
 -- GUNAKAN DATABASE
 
 USE UAS;
+
+--- DML(INSERT)
+-- INSERT DATA MASTER
+
+
+INSERT INTO jenisBarang VALUES
+(1,'Alat Makan'),
+(2,'Tempat Minum'),
+(3,'Toples');
+
+INSERT INTO barang VALUES
+(134,'Tupperware kecil 300ml',50,4800,2),
+(146,'Tupperware sedang 600ml',20,7500,2),
+(147,'Tupperware besar 1L',19,12000,2),
+(148,'Gelas plastik bening',12,15000,2),
+(149,'Piring plastik',6,20000,1),
+(150,'Sendok plastik',12,10000,1),
+(151,'Botol minum plastik 500ml',1,10000,2),
+(152,'Toples plastik kecil',5,9000,3),
+(153,'Toples plastik besar',8,15000,3),
+(154,'Wadah makanan sekat',11,13000,1);
+
+INSERT INTO kasir VALUES
+(321879,'MAJU KASTALAN','081225064867');
+
+INSERT INTO sales VALUES
+(8629026,'DHEA','Karyawan');
+
+INSERT INTO pelanggan VALUES
+('AFD34','Pelanggan Umum','087786549984');
+
+INSERT INTO vendor (idVendor,namaVendor,alamatVendor,idBarang,idjenisBarang) VALUES
+(123000,'ADAFIVENDORS','Jakarta',146,2),
+(123000,'ADAFIVENDORS','Jakarta',147,2),
+(223000,'CV. JAYA PLASTIK','Bandung',148,2),
+(223000,'CV. JAYA PLASTIK','Bandung',149,1),
+(223000,'CV. JAYA PLASTIK','Bandung',150,1),
+(223000,'CV. JAYA PLASTIK','Bandung',151,2),
+(323000,'TOKO ANEKA WADAH','Surabaya',152,3),
+(323000,'TOKO ANEKA WADAH','Surabaya',153,3),
+(323000,'TOKO ANEKA WADAH','Surabaya',154,1);
+
+INSERT INTO transaksi VALUES
+('JP/001/001/20251108/150535','2025-11-08',4800,20000,15000,1,154,4800,321879,8629026,'AFD34');
+
 --- DML (SELECT DASAR-JOIN)
 -- 1. SELECT DASAR
 
